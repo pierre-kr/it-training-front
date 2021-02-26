b827ab2d6e13628350ec1709f28e8a90ac643294
Pierre Krepa <pierre.krepa@hotmail.com>
2021-02-26 22:40:19 +0100

    Construction d'un composant de formation en dur

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormationComponent } from './components/formation/formation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"formation", component:FormationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
