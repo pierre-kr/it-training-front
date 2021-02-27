5fb867ffa51adde9ae1c3b2c51afdf1033a49662
quentinH-C <quentinhorgnies@gmail.com>
2021-02-27 23:13:12 +0100

    premiere version affichage formation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormationComponent } from './pages/formation/formation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'formation', component:  FormationComponent},
+  {path: '**', redirectTo: 'formation'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
